211b9e65743d24605372ef4f25f3b8038ae28f85
NagulanD <151419441+NagulanD@users.noreply.github.com>
2024-12-30 16:17:25 +0530

    Update App.jsx

--- Slam_book/src/App.jsx
+++ Slam_book/src/App.jsx
@@ -1,20 +1,20 @@
-import { Component } from "react"
-import Form from './Form.jsx'
-import './App.css'
-import Slambook from "./Slambook.jsx"
+// import { Component } from "react"
+// import Form from './Form.jsx'
+// import './App.css'
+// import Slambook from "./Slambook.jsx"
 
-class App extends Component{
+// class App extends Component{
 
-  render(){
-    return(
-      <> 
-         <Slambook></Slambook>
-      </>
-    )
-  }
-}
+//   render(){
+//     return(
+//       <> 
+//          <Slambook></Slambook>
+//       </>
+//     )
+//   }
+// }
 
-export default App
+// export default App
 
 // 
 // import Form from './Form.jsx'
@@ -30,3 +30,50 @@ export default App
 // }
 
 // export default App
+
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <Router>
+      <table className="layout-table">
+        <thead>
+          <tr>
+            <td className="logo-cell"><Link to="/home">O ABC</Link></td>
+            <td colSpan="3" className="nav-cell">
+              <nav>
+                <ul className="nav-links">
+                  <li><Link to="/home">HOME</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                  <li><Link to="/contact">Contact</Link></li>
+                </ul>
+              </nav>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colSpan="4" className="content-cell">
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="4" className="footer-cell">copyright</td>
+          </tr>
+        </tfoot>
+      </table>
+    </Router>
+  );
+}
+
+export default App;
